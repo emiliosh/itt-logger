a0c2ace3dbbdf8234d02b686504f585f000e34b2
legik777 <75944488+legik777@users.noreply.github.com>
2023-08-04 14:28:03 +0300

    Create ittlogger.php

---
+++ config/ittlogger.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'batch' => false,
+    'queue' => [
+        'enable' => false,
+        'name' => env('LOG_QUEUE_NAME', 'logging')
+    ],
+    'level' => 200,
+    'bubble' => false,
+    'opensearch' => [
+        'hosts' => [
+            [
+                'host' => env('OPENSEARCH_HOST', 'localhost'),
+                'port' => env('OPENSEARCH_PORT', 9200),
+                'scheme' => env('OPENSEARCH_SCHEME', 'http'),
+                'user' => env('OPENSEARCH_USER', null),
+                'pass' => env('OPENSEARCH_PASS', null)
+            ],
+        ],
+        'retries' => 2,
+        'cert' => '',
+        'params' => [
+            'client' => [
+                'timeout' => 2,
+                'connect_timeout' => 2
+            ]
+        ]
+    ],
+
+    'options' => [
+        'index' => strtolower(env('APP_NAME', 'laravel')),
+        'type' => '_doc',
+        'ignore_error' => false,
+    ],
+    'exception' => [
+        'trace' => false,
+    ],
+    'extra' => [
+        'host' => env('APP_URL'),
+        'php' => PHP_VERSION,
+        'laravel' => app()->version(),
+        'env' => env('APP_ENV')
+    ],
+];
